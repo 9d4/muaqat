b24e7034e87f6481603cca9cbf03861b74ab5ca1
traper <traperwaze@gmail.com>
2022-02-28 10:39:11 +0700

    interface brief

--- main.go
+++ main.go
@@ -1,5 +1,33 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"time"
+)
 
+type TestIface interface {
+	Greet() string
+}
+
+type Thing struct {}
+
+func (i *Thing) Greet() string {
+	return "Holla"
+}
+
+
+func main() {
+	if i := Test(); i != nil {
+		fmt.Println(i.Greet())
+	}
+}
+
+func Test() TestIface {
+	now := time.Now().Unix()
+
+	if now % 2 == 0{
+		return &Thing{}
+	}
+
+	return nil
 }
