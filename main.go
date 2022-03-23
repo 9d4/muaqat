93f2e75f7ddf8beff997c825098fe9c9d58e30ad
Dimas Ananda <traperwaze@gmail.com>
2022-03-23 09:28:23 +0700

    simple automation create vars from api endpoints
    Signed-off-by: Dimas Ananda <traperwaze@gmail.com>

--- main.go
+++ main.go
@@ -1,33 +1,36 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"time"
+	"io"
+	"os"
+	"strings"
 )
 
-type TestIface interface {
-	Greet() string
-}
-
-type Thing struct {}
-
-func (i *Thing) Greet() string {
-	return "Holla"
-}
-
-
 func main() {
-	if i := Test(); i != nil {
-		fmt.Println(i.Greet())
-	}
-}
-
-func Test() TestIface {
-	now := time.Now().Unix()
-
-	if now % 2 == 0{
-		return &Thing{}
+	file, err := os.Open("./endpoints.txt")
+	if err != nil {
+		panic(err)
 	}
 
-	return nil
+	buf := bufio.NewReader(file)
+	for {
+		line, err := buf.ReadString('\n')
+		if err == io.EOF {
+			break
+		}
+
+		s := strings.Split(line, "/")
+
+		for i, word := range s {
+			if i == 0 {
+				continue
+			}
+
+			f := strings.ToUpper(string(word[0]))
+			final := f + word[1:]
+			fmt.Print(final)
+		}
+	}
 }
