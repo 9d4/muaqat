8f87ea0e0b239278b8b15a585c08301882936125
Dimas Ananda <traperwaze@gmail.com>
2022-06-09 09:36:19 +0700

    test request to ladoosingh
    Signed-off-by: Dimas Ananda <traperwaze@gmail.com>

--- main.go
+++ main.go
@@ -1,42 +1,71 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
-	"io"
-	"os"
-	"strings"
+	"log"
+	"net"
+	"sync"
+	"time"
 )
 
+var (
+	host string = "ladoosingh.herokuapp.com:80"
+	path string = "/l/kVFQaOMXYpm"
+	method string = "GET"
+	routines int = 100
+)
+
+var wg = sync.WaitGroup{}
+
 func main() {
-	file, err := os.Open("./endpoints.txt")
-	if err != nil {
-		panic(err)
-	}
+	sendRequest := func(path string, method string, start chan int8) {
+		wg.Add(1)
+		num := <-start
 
-	buf := bufio.NewReader(file)
-	for {
-		line, err := buf.ReadString('\n')
-		if err == io.EOF {
-			break
-		}
-
-		line = strings.ReplaceAll(line, string('\n'), "")
-		s := strings.Split(line, "/")
-
-		final := ""
-		for i, word := range s {
-			if i == 0 {
-				continue
+		fn := func ()  {
+			// open tcp connection to url
+			conn, err := net.Dial("tcp", host)
+			if err != nil {
+				log.Fatal(err)
 			}
 
-			f := strings.ToUpper(string(word[0]))
-			final += f + word[1:]
+			// send request
+			conn.Write([]byte(method + " " + path + " HTTP/1.1\r\n"))
+			conn.Write([]byte("Host: " + host + "\r\n"))
+			conn.Write([]byte("Connection: close\r\n"))
+			conn.Write([]byte("\r\n"))
+			log.Println("sent. Routine:", num)
 		}
 
-		final += " = " + "\"" + line + "\""
-
-		fmt.Println(final)
-		// break
+		// make a ticker
+		ticker := time.NewTicker(time.Millisecond * 100)
+		
+		for {
+			<-ticker.C
+			fn()
+		}
 	}
+
+	var rs []chan int8
+
+	// make routines
+	for i := 0; i < routines; i++ {
+		start := make(chan int8)
+		rs = append(rs, start)
+
+		go (func() {
+			// send request
+			sendRequest(path, method, start)
+		})()
+	}
+
+	log.Println("Routines Created!")
+	// set delay 5s
+	time.Sleep(time.Second * 3)
+	log.Println("Starting Routines!")
+
+	// start all routines
+	for i, r := range rs {
+		r <- int8(i) 
+	}
+	wg.Wait()
 }
