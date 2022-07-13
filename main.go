7a29080d3a2e592485e891e7c60ca247928e316d
Dimas Ananda <traperwaze@gmail.com>
2022-07-13 08:01:27 +0700

    restore | clean up
    Signed-off-by: Dimas Ananda <traperwaze@gmail.com>

--- main.go
+++ main.go
@@ -1,16 +1,5 @@
 package main
 
-import (
-	"os"
-)
-
 func main() {
-	var hanan Tulisan = "꧋ꦲꦤꦕꦫꦏ꧉"
-	hanan.Write();
-}
 
-type Tulisan string
-
-func (t Tulisan) Write() {
-	os.Stdout.Write([]byte(t))
 }
