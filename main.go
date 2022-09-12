2f2f46ec32a95bc0a4192b8867b88ea84a1a1b90
Dimas Ananda traper <traperwaze@gmail.com>
2022-09-12 08:33:05 +0700

    net listen tcp test

--- main.go
+++ main.go
@@ -1,5 +1,39 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"log"
+	"net"
+)
 
+func main() {
+	listener, err := net.Listen("tcp", "localhost:8080")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			continue
+		}
+
+		go handleConn(conn)
+	}
+}
+
+func handleConn(conn net.Conn) {
+	reader := bufio.NewReader(conn)
+
+	for {
+		bytes, err := reader.ReadBytes('\n')
+
+		if err != nil {
+			break
+		}
+
+		log.Println(string(bytes))
+	}
+
+	conn.Close()
 }
