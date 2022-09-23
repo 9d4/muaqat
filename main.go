32d67c4295fe44e4e5737ea338c2a124ae3cc142
Dimas Ananda traper <traperwaze@gmail.com>
2022-09-24 05:42:51 +0700

    clean up

--- main.go
+++ main.go
@@ -1,39 +1,4 @@
 package main
 
-import (
-	"bufio"
-	"log"
-	"net"
-)
-
 func main() {
-	listener, err := net.Listen("tcp", "localhost:8080")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	for {
-		conn, err := listener.Accept()
-		if err != nil {
-			continue
-		}
-
-		go handleConn(conn)
-	}
-}
-
-func handleConn(conn net.Conn) {
-	reader := bufio.NewReader(conn)
-
-	for {
-		bytes, err := reader.ReadBytes('\n')
-
-		if err != nil {
-			break
-		}
-
-		log.Println(string(bytes))
-	}
-
-	conn.Close()
 }
